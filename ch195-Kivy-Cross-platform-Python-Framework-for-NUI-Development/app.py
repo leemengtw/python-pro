08a0e1b40000eef1b223c38db185c37cf6e4d48b
leemengtaiwan <b98705001@gmail.com>
2018-01-21 03:15:02 +0900

    Add naive hello world app

---
+++ ch195-Kivy-Cross-platform-Python-Framework-for-NUI-Development/app.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class Test(App):
+    def build(self):
+        return Label(text='Hello world')
+
+if __name__ == '__main__':
+    Test().run()
